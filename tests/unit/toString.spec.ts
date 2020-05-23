e5d355b3a58f3b782ab17d9c3bd528d31cb61709
nkjmsss <nakajima-sota018@g.ecc.u-tokyo.ac.jp>
2020-05-23 21:24:23 +0900

    :white_check_mark: Add complicated tests

--- tests/unit/toString.spec.ts
+++ tests/unit/toString.spec.ts
@@ -31,14 +31,11 @@ describe('lib/markdown', () => {
       })
 
       test('ins', check('++foobar++'))
-
       test('mark', check('==foobar=='))
-
       test('s', check('~~foobar~~'))
 
       describe('link', () => {
         test('normal', check('[link](https://example.com)'))
-
         test('with title', check('[link](https://example.com "title")'))
       })
     })
@@ -47,6 +44,25 @@ describe('lib/markdown', () => {
     describe('block', () => {
       describe('headings', () => {
         test('h1', check('# heading'))
+        test('h2', check('## heading'))
+        test('h3', check('### heading'))
+        test('h4', check('#### heading'))
+        test('h5', check('##### heading'))
+        test('h6', check('###### heading'))
+      })
+    })
+
+    // complicated
+    describe('complicated', () => {
+      describe('mixed inline rules', () => {
+        test('text + strong', check('sample**foo**sample__foo__'))
+        test('link + text + s', check('[link](https://example.com "title")text~~strike~~'))
+      })
+
+      describe('nested inline rules', () => {
+        test('strong > ins', check('**strong++ins++**'))
+        test('link > em', check('[link*em*](https://example.com "title")'))
+        test('em > strike', check('_~~strike~~em_'))
       })
     })
   })
