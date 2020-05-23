29c8c743e06f0af4bc1007b8860af0dc6881c333
nkjmsss <nakajima-sota018@g.ecc.u-tokyo.ac.jp>
2020-05-23 13:03:09 +0900

    :sparkles: Enabled to pass md options

--- src/components/VPreviewer/index.vue
+++ src/components/VPreviewer/index.vue
@@ -1,7 +1,16 @@
 <script lang="tsx">
 import { Components, components, fallbackComponent } from './Renderers'
-import { MarkdownItToken, Token, createAST, markdown } from '@/lib/markdown'
+import { MarkdownItToken, Token, createAST } from '@/lib/markdown'
+import MarkdownIt from 'markdown-it'
 import Vue, { PropType, VueConstructor } from 'vue'
+import ins from 'markdown-it-ins'
+import mark from 'markdown-it-mark'
+import md from 'markdown-it'
+
+type MarkdownItUse<T = any> =
+  | ((plugin: MarkdownIt.PluginSimple) => MarkdownIt)
+  | ((plugin: MarkdownIt.PluginWithOptions<T>, options?: T) => MarkdownIt)
+  | ((plugin: MarkdownIt.PluginWithParams, ...params: any[]) => MarkdownIt)
 
 export default Vue.extend({
   name: 'VPreviewer',
@@ -15,17 +24,61 @@ export default Vue.extend({
       type: Array as PropType<Components>,
       default: () => [],
     },
+    mdOptions: {
+      type: Object as PropType<Partial<md.Options>>,
+      default: () => ({}),
+    },
+    mdDisables: {
+      type: Array as PropType<string[]>,
+      default: () => [],
+    },
+    mdPlugins: {
+      type: Array as PropType<Array<Parameters<MarkdownItUse>>>,
+      default: () => [],
+    },
   },
 
   computed: {
+    markdown(): MarkdownIt {
+      const markdown = md({
+        html: false,
+        xhtmlOut: false,
+        breaks: false,
+        langPrefix: 'lang-',
+        linkify: false,
+        typographer: true,
+        quotes: '“”‘’',
+        ...this.mdOptions,
+      })
+
+      markdown
+        .disable('lheading') // a special syntax for h1 and h2 (using "=" or "-" below the text)
+        .disable('code') // four space to make code block
+
+      markdown //
+        .use(ins)
+        .use(mark)
+
+      this.mdDisables.forEach(rule => {
+        markdown.disable(rule)
+      })
+
+      this.mdPlugins.forEach(plugin => {
+        const [p, ...opts] = plugin
+        markdown.use(p, ...opts)
+      })
+
+      return markdown
+    },
+
     tokens(): MarkdownItToken[] {
-      return markdown.parse(this.value, {})
+      return this.markdown.parse(this.value, {})
     },
 
     Tokens(): Token[] {
       return createAST({
         src: this.value,
-        tokenizer: (src: string) => markdown.parse(src, {}),
+        tokenizer: (src: string) => this.markdown.parse(src, {}),
       })
     },
   },
